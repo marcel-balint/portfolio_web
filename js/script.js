9dbd885df351e24cb3cd1a2e650364a00522b114
Marcel Balint <marcelbalint363@gmail.com>
2023-10-07 12:15:27 +0300

    Display an 'X' when menu is open so user can close the menu

--- js/script.js
+++ js/script.js
@@ -1,15 +1,20 @@
 const menuIcon = document.querySelector(".mobile-icon");
 const navigationLinks = document.querySelector(".nav-links");
 const navigationItems = document.querySelectorAll(".nav-links li");
+const navigation = document.querySelector("nav");
 
 document.querySelector("video").playbackRate = 0.3;
 
 navigationItems.forEach((el) => {
   el.addEventListener("click", () => {
     navigationLinks.classList.remove("active");
+    menuIcon.classList.remove("active");
+    navigation.classList.toggle("active");
   });
 });
 
 menuIcon.addEventListener("click", () => {
+  menuIcon.classList.toggle("active");
   navigationLinks.classList.toggle("active");
+  navigation.classList.toggle("active");
 });
