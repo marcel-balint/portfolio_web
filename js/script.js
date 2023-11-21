11da432101380012927d9f32abcbbfd6dc6e6b13
Marcel Balint <45772587+marcel-balint@users.noreply.github.com>
2023-11-21 16:51:29 +0200

    Created a modal for project description

--- js/script.js
+++ js/script.js
@@ -3,6 +3,9 @@ const navigationLinks = document.querySelector(".nav-links");
 const navigationItems = document.querySelectorAll(".nav-links li");
 const navigation = document.querySelector("nav");
 const swiperEl = document.querySelector("swiper-container");
+const readMoreBtn = document.querySelector(".read-more");
+const modal = document.querySelector(".modal");
+const closeBtn = document.querySelector(".close");
 
 navigationItems.forEach((el) => {
   el.addEventListener("click", () => {
@@ -16,6 +19,22 @@ menuIcon.addEventListener("click", () => {
   menuIcon.classList.toggle("active");
   navigationLinks.classList.toggle("active");
   navigation.classList.toggle("active");
+});
+
+// Modal functionality
+
+readMoreBtn.addEventListener("click", () => {
+  modal.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.classList.remove("active");
+});
+
+modal.addEventListener("click", (e) => {
+  if (e.target.classList.contains("active")) {
+    modal.classList.remove("active");
+  }
 });
 
 // Swiper styles
@@ -36,3 +55,27 @@ nextBtn.style.marginRight = "20px";
 nextBtn.style.borderRadius = "20px";
 nextBtn.style.color = "#000";
 nextBtn.style.backgroundColor = "rgba(255,255,255, 0.7)";
+
+const paginationBullets = swiperEl.swiper.el.children[5].children;
+for (const bullet of paginationBullets) {
+  console.log(bullet);
+  bullet.style.width = ".7rem";
+  bullet.style.height = ".7rem";
+}
+
+// Adjust styles for smaller screens
+if (window.screen.width < 760) {
+  prevBtn.style.top = "37%";
+  prevBtn.style.padding = "0px";
+  nextBtn.style.padding = "0px";
+  nextBtn.style.top = "37%";
+}
+
+window.addEventListener("resize", (event) => {
+  if (window.screen.width < 760) {
+    prevBtn.style.top = "37%";
+    prevBtn.style.padding = "0px";
+    nextBtn.style.padding = "0px";
+    nextBtn.style.top = "37%";
+  }
+});
