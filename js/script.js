b88143876fd0325bc1d34b470b9f3e4e9e301a52
Marcel Balint <45772587+marcel-balint@users.noreply.github.com>
2023-11-21 13:49:10 +0200

    Added a swiper

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const menuIcon = document.querySelector(".mobile-icon");
 const navigationLinks = document.querySelector(".nav-links");
 const navigationItems = document.querySelectorAll(".nav-links li");
 const navigation = document.querySelector("nav");
+const swiperEl = document.querySelector("swiper-container");
 
 navigationItems.forEach((el) => {
   el.addEventListener("click", () => {
@@ -16,3 +17,22 @@ menuIcon.addEventListener("click", () => {
   navigationLinks.classList.toggle("active");
   navigation.classList.toggle("active");
 });
+
+// Swiper styles
+const prevBtn = swiperEl.swiper.el.children[3];
+prevBtn.style.top = "45%";
+prevBtn.style.height = "27px";
+prevBtn.style.padding = "5px 7px 5px 5px";
+prevBtn.style.marginLeft = "20px";
+prevBtn.style.borderRadius = "20px";
+prevBtn.style.color = "#000";
+prevBtn.style.backgroundColor = "rgba(255,255,255, 0.7)";
+
+const nextBtn = swiperEl.swiper.el.children[4];
+nextBtn.style.top = "45%";
+nextBtn.style.height = "27px";
+nextBtn.style.padding = "5px 5px 5px 7px";
+nextBtn.style.marginRight = "20px";
+nextBtn.style.borderRadius = "20px";
+nextBtn.style.color = "#000";
+nextBtn.style.backgroundColor = "rgba(255,255,255, 0.7)";
