4e1f18408644d6adba97bd8d112a4d5dd33e7c82
Marcel Balint <45772587+marcel-balint@users.noreply.github.com>
2023-11-24 19:55:00 +0200

    Added projects to the protfolio

--- js/script.js
+++ js/script.js
@@ -3,9 +3,34 @@ const navigationLinks = document.querySelector(".nav-links");
 const navigationItems = document.querySelectorAll(".nav-links li");
 const navigation = document.querySelector("nav");
 const swiperEl = document.querySelector("swiper-container");
-const readMoreBtn = document.querySelector(".read-more");
+const readMoreBtn = document.querySelectorAll(".read-more");
 const modal = document.querySelector(".modal");
 const closeBtn = document.querySelector(".close");
+const currentYearEl = document.querySelector(".current-year");
+const projects = [
+  {
+    className: "equity",
+    title: "EquityEdge",
+    description:
+      "Unique futures trading platform for both new and experienced traders to practice trading in a simulated environment before entering the real stock market.",
+    technologies: "HTML, CSS, JavaScript.",
+  },
+  {
+    className: "push",
+    title: "Push",
+    description:
+      "Real estate platform built as part of the final project of Coding Bootcamp Praha. Users are able to list their properties for renting or selling and receive interest requests from buyers/renters.",
+    technologies: "SCSS, React.js (Context API),  Laravel.",
+  },
+  {
+    className: "countries",
+    title: "World Countries",
+    description:
+      "Integrated REST Countries API to pull countries data and display it on pages, applied dark mode feature, search, filtering options and pagination.",
+
+    technologies: "CSS, React.js, React Router.",
+  },
+];
 
 navigationItems.forEach((el) => {
   el.addEventListener("click", () => {
@@ -22,9 +47,22 @@ menuIcon.addEventListener("click", () => {
 });
 
 // Modal functionality
-
-readMoreBtn.addEventListener("click", () => {
-  modal.classList.add("active");
+readMoreBtn.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    let projectTitle = modal.querySelector("h2");
+    let projectDescription = modal.querySelector(".project-desciption");
+    let projectTechnologies = modal.querySelector(".technologies");
+    // Add content based on class
+    const project = projects.find((proj) =>
+      e.target.classList.contains(proj.className)
+    );
+    if (project) {
+      projectTitle.textContent = project.title;
+      projectDescription.textContent = project.description;
+      projectTechnologies.textContent = project.technologies;
+    }
+    modal.classList.add("active");
+  });
 });
 
 closeBtn.addEventListener("click", () => {
@@ -36,6 +74,10 @@ modal.addEventListener("click", (e) => {
     modal.classList.remove("active");
   }
 });
+
+const date = new Date();
+const year = date.getFullYear();
+currentYearEl.textContent = year;
 
 // Swiper styles
 const prevBtn = swiperEl.swiper.el.children[3];
@@ -58,7 +100,6 @@ nextBtn.style.backgroundColor = "rgba(255,255,255, 0.7)";
 
 const paginationBullets = swiperEl.swiper.el.children[5].children;
 for (const bullet of paginationBullets) {
-  console.log(bullet);
   bullet.style.width = ".7rem";
   bullet.style.height = ".7rem";
 }
@@ -71,7 +112,7 @@ if (window.screen.width < 760) {
   nextBtn.style.top = "37%";
 }
 
-window.addEventListener("resize", (event) => {
+window.addEventListener("resize", () => {
   if (window.screen.width < 760) {
     prevBtn.style.top = "37%";
     prevBtn.style.padding = "0px";
